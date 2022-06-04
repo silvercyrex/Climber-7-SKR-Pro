355f2b46f2745d385f00554ea30965e28a63edb1
Sina <github@sinarastegar.com>
2022-06-04 12:09:31 -0400

    Fixed a Typo

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -96,7 +96,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define BOARD_BTT_SKR_PRO_V1_1 //MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_BTT_SKR_PRO_V1_1 //BOARD_RAMPS_14_EFB
 #endif
 
 /**
